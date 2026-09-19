246be6f92a525e05c1186a4fc29a6ed6aa818ff2
syncev <santiagonhernandez@gmail.com>
2026-09-19 11:40:48 -0300

    estructura de archivo y tamaño de ventana de arranque

---
+++ app/application.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+from screeninfo import get_monitors
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # VARIABLES
+
+        # toma las dimensiones del monitor principal para que la ventana se ajuste a la pantalla
+        monitors = get_monitors()
+        principal_monitors = monitors[0]
+        screen_width = principal_monitors.width
+        screen_height = principal_monitors.height
+
+        # modifica esas dimensiones para que no sea arranque en pantalla completa
+        app_width = int(screen_width * 0.7)
+        app_height = int(screen_height * 0.8)
+        
+        # basicas de ventana principal
+        self.geometry(f"{app_width}x{app_height}")
+        self.resizable(True, True)
+        self.configure(bg="white")
+        self.title("Vet Logic")
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from app.application import App
 
 if __name__ == "__main__":
     app = App()
+    
     app.mainloop()
