6adcfa7dbdf82ab28bcdace03d71a182f0ed4ced
syncev <santiagonhernandez@gmail.com>
2026-09-19 20:56:57 -0300

    agregada pantalla de login en su propio archivo y carpeta

--- app/application.py
+++ app/application.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 from screeninfo import get_monitors
 
+from ui.login_view import LoginView
+
 
 class App(tk.Tk):
     def __init__(self):
@@ -18,9 +20,13 @@ class App(tk.Tk):
         # modifica esas dimensiones para que no sea arranque en pantalla completa
         app_width = int(screen_width * 0.7)
         app_height = int(screen_height * 0.8)
-        
+
         # basicas de ventana principal
         self.geometry(f"{app_width}x{app_height}")
         self.resizable(True, True)
         self.configure(bg="white")
         self.title("Vet Logic")
+
+
+        self.login_view = LoginView(self)
+        self.login_view.pack()
---
+++ ui/login_view.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+
+
+class LoginView(tk.Frame):
+    def __init__(self, master, **kwargs):
+        kwargs.setdefault("bg", "white")
+        super().__init__(master, **kwargs)
+        self._build_widgets()
+        self._build_layout()
+
+        # WIDGETS
+
+    def _build_widgets(self):
+        # titles
+        self.title = tk.Label(
+            self,
+            text="Vet Logic",
+            font=("Inter", -64, "bold"),
+            bg="white",
+        )
+        self.subtitle = tk.Label(
+            self,
+            text="Gestión veterinaria",
+            font=("Inter", -24, "italic"),
+            bg="white",
+        )
+        # input wrapper
+        self.frame_inputs = tk.Frame(
+            self,
+            width=525,
+            height=488,
+            bg="#D9D9D9",
+        )
+        # inputs
+        self.name_label = tk.Label(
+            self.frame_inputs,
+            text="Usuario",
+            font=("Inter", -32, "bold"),
+            bg="#D9D9D9",
+        )
+        self.name_entry = tk.Entry(
+            self.frame_inputs,
+            bd=0,
+            width=45,
+            bg="white",
+        )
+
+        self.password_label = tk.Label(
+            self.frame_inputs,
+            text="Contraseña",
+            font=("Inter", -32, "bold"),
+            bg="#D9D9D9",
+        )
+        self.password_entry = tk.Entry(
+            self.frame_inputs,
+            bd=0,
+            width=45,
+            
+            bg="white",
+            show="*",
+        )
+        # login button
+        self.login_button = tk.Button(
+            self.frame_inputs,
+            text="Ingresar",
+            width=20,
+            bg="#8938D6",
+            font=(
+                "Inter",
+                -32,
+                
+            ),
+            fg="white"
+        )
+
+        # LAYOUT
+
+    def _build_layout(self):
+        self.title.pack(pady=(75, 0))
+        self.subtitle.pack()
+        self.frame_inputs.pack(pady=(150, 0))
+        self.frame_inputs.pack_propagate(
+            False
+        )  # Evita que el frame se ajuste al tamaño de sus hijos
+        self.name_label.pack(pady=(77, 0))
+        self.name_entry.pack()
+        self.password_label.pack(pady=(31, 0))
+        self.password_entry.pack()
+        self.login_button.pack(pady=(78, 0))
