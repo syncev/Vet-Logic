6f67d7d35d2ea7bf60c34992169a75d8557a4eb6
syncev <santiagonhernandez@gmail.com>
2026-09-20 15:39:03 -0300

    Feat: llamado widget de searchbar en historias clinicas

--- ui/pages/historias/page.py
+++ ui/pages/historias/page.py
@@ -1,14 +1,18 @@
 import tkinter as tk
+from ui.widgets.searchbar import Searchbar
 
 
 class HistoriasPage(tk.Frame):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
 
-        # Título
-        title = tk.Label(
-            self,
-            text="Historias",
-            font=("Inter", -32)
-        )
-        title.pack()
+        self._build_widgets()
+        self._build_layout()
+
+
+
+    def _build_widgets(self):
+        self.searchbar = Searchbar(self)
+
+    def _build_layout(self):
+        self.searchbar.pack(pady=10, padx=10, fill="x")
