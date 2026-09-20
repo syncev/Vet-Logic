f6d144c06f8508410feba0f00373af1e7f2a7ffa
syncev <santiagonhernandez@gmail.com>
2026-09-20 14:48:56 -0300

    fix: boton de agregar turno no habia sido puesto en layout

--- ui/pages/turnero/page.py
+++ ui/pages/turnero/page.py
@@ -3,6 +3,8 @@ from zoneinfo import ZoneInfo
 import tkinter as tk
 
 from ui.widgets.searchbar import Searchbar
+
+
 class TurneroPage(tk.Frame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
@@ -10,34 +12,34 @@ class TurneroPage(tk.Frame):
         self._build_widgets()
         self._build_layout()
 
-
     def _build_date_stepper(self):
         self.date_stepper = tk.Frame(self)
         timezone = ZoneInfo("America/Argentina/Buenos_Aires")
 
+        # logca para que los dias se sumen sin pasar de 31 a 32
         today = datetime.now(timezone)
         tomorrow = today + timedelta(days=1)
         next_day = today + timedelta(days=2)
 
         self.previous_button = tk.Button(
             self.date_stepper,
-            text="<"
+            text="<",
         )
         self.today_button = tk.Button(
             self.date_stepper,
-            text = str(today.day)
+            text=str(today.day),
         )
         self.tomorrow_button = tk.Button(
             self.date_stepper,
-            text= str(tomorrow.day)
+            text=str(tomorrow.day),
         )
         self.next_day_button = tk.Button(
             self.date_stepper,
-            text=str(next_day.day)
+            text=str(next_day.day),
         )
         self.next_button = tk.Button(
             self.date_stepper,
-            text=">"
+            text=">",
         )
 
     def _build_widgets(self):
@@ -47,22 +49,15 @@ class TurneroPage(tk.Frame):
 
         self.add_appointment_button = tk.Button(
             self,
-            text="+ Agregar Turno"
+            text="+ Agregar Turno",
         )
 
     def _build_layout(self):
-        self.searchbar.pack(
-            fill="x",
-            padx=40,
-            pady=30
-        )
+        self.searchbar.pack(fill="x", padx=40, pady=30)
         self.date_stepper.pack()
         self.previous_button.pack(side="left")
         self.today_button.pack(side="left")
         self.tomorrow_button.pack(side="left")
         self.next_day_button.pack(side="left")
         self.next_button.pack(side="left")
-
-
-
-
+        self.add_appointment_button.pack()
